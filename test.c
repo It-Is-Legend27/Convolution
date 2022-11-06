07682be5628c1e963b729ba90ef2511e7a29a8bf
Angel Badillo <badilloa022402@gmail.com>
2022-11-06 14:28:09 -0600

    testing output again

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 //**************************************************************
-// This is just to test the sample output
+// This is just to test the output
 //*****************************************************************
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +11,7 @@
 #define N 3
 
 // Number of elements in H
-#define O N+M-1
+#define O N + M - 1
 
 // Size of buffer to hold formatted string
 #define STR_SZ 128
@@ -25,17 +25,17 @@ int main()
 {
     // For use to hold formatted string
     string_buffer output_string;
-    
+
     // Create (if does not exist) and open file for write
-    FILE* outputFilePtr = fopen("test.csv", "w");
-    
+    FILE *outputFilePtr = fopen("test.csv", "w");
+
     // Create arrays for sequences F, G, and resulting H
-    int F[M] = {1,2,3,4,5};
-    int G[N] = {5,6,7};
-    int H[O] = {0};
+    long long F[M] = {1,2,3,4,5};
+    long long G[N] = {5,6,7};
+    long long H[O] = {0};
 
     // Checksum to be calculated once H is computed
-    int checkSum = 0;
+    long long checkSum = 0;
 
     // Computes all values of H(n) from 0 up to N+M-2
     for (int n = 0; n < O; n++)
@@ -46,31 +46,34 @@ int main()
         {
             // Only performed if indices are valid.
             // If m < 0 and m >= M and n < 0 and n <= N, this
-            // instruction will be skipped as the item to be added to the sum
+            // instruction will be skipped as the value to be added to the sum
             // will be 0.
-            if(m >= 0 && m < M && (n-m) >= 0 && (n-m) < N)
-            H[n] += F[m]*G[n-m];
+            if (m >= 0 && m < M && (n - m) >= 0 && (n - m) < N)
+                H[n] += F[m] * G[n - m];
         }
+        // Add to check sum
+        checkSum += H[n];
     }
-    
+
     // Printing number of elements in each array of values
     printf("Number of elements in f: %d\n", M);
     printf("Number of elements in g: %d\n", N);
     printf("Number of elements in h: %d\n", O);
+    printf("Checksum results: %lld\n", checkSum);
 
-     // Print labels for columns to .csv file
-    fputs("H[i], value\n", outputFilePtr);
+    // Print labels for columns to .csv file
+    fputs("n, H[n]\n", outputFilePtr);
 
     // Print every element of H to .csv file
     for (int i = 0; i < O; i++)
     {
         // Create formatted string, store in buffer, output_string
-        sprintf(output_string, "H[%d], %d\n", i, H[i]);
+        sprintf(output_string, "%d, %lld\n", i, H[i]);
 
         // Print output_string to .csv file
         fputs(output_string, outputFilePtr);
     }
-    
+
     // Close the file
     fclose(outputFilePtr);
 }
